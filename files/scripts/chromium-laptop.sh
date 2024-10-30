29105c1f11257fffaa54d87db8e215e10037a8ef
mkkvcs <148012377+mkkvcs@users.noreply.github.com>
2024-10-30 02:03:52 +0100

    Update chromium-laptop.sh

--- files/scripts/chromium-laptop.sh
+++ files/scripts/chromium-laptop.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 set -oue pipefail
 sed -i 's/--enable-features=/--enable-features=TouchpadOverscrollHistoryNavigation,/' /etc/chromium/chromium.conf
-sed -i 's/GRAPHIC_DRIVER=default/GRAPHIC_DRIVER=intel/' /etc/chromium/chromium.conf
